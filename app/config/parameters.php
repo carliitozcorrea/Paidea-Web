ec05a370da370135a2dbc851ac61c9797a716083
carlos <carlosx-34@hotmail.com>
2017-05-15 01:52:14 -0500

    Heroku config

---
+++ app/config/parameters.php
@@ -0,0 +1,11 @@
+<?php
+$db = parse_url(getenv('DATABASE_URL'));
+
+if ($db)
+{
+    $container->setParameter('database_host', $db['host']);
+    $container->setParameter('database_port', $db['port']);
+    $container->setParameter('database_name', substr($db["path"], 1));
+    $container->setParameter('database_user', $db['user']);
+    $container->setParameter('database_password', $db['pass']);
+}
